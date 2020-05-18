c5566524008bda364e3e65c09164c2e1e5bbd9ce
flukie123 <60082386+flukie123@users.noreply.github.com>
2020-05-18 22:55:46 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+var FirebaseKeys = {
+    apiKey: "AIzaSyAI6PsJ2yXme0U47jMySn2UbeVuWRY0ofg",
+    authDomain: "beauty-080852.firebaseapp.com",
+    databaseURL: "https://beauty-080852.firebaseio.com",
+    projectId: "beauty-080852",
+    storageBucket: "beauty-080852.appspot.com",
+    messagingSenderId: "40957405422",
+    appId: "1:40957405422:web:290e38e25e98bfcd69abff",
+    measurementId: "G-JH5Z6CH314"
+};
+
+export default FirebaseKeys;
